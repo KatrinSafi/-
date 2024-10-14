e2b450ac241f4a3c2c70d56429c33175190a058d
KatrinSafi <167858422+KatrinSafi@users.noreply.github.com>
2024-10-14 21:52:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+course = "Python"
+print(course)
+total_tasks = 12
+print(total_tasks)
+hours_spent = 1.5
+print(hours_spent)
+average_execution_time = 0.125
+print("Course:", course, "total_tasks:",total_tasks, "hours_spent:",hours_spent,"average_execution_time:",average_execution_time)
